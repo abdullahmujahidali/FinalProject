7686387d66ff6a9e50ce3fbff6dd2f1d26a88cda
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2020-10-26 22:26:05 +0500

    added jwt and extracted token from it

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://abdullah:abdullah@projectcluster.dsmrv.mongodb.net/bigbrains?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://abdullah:abdullah@projectcluster.dsmrv.mongodb.net/bigbrains?retryWrites=true&w=majority",
+    JWT_SECRET: "bigisbrain123"
 }
