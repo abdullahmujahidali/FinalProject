143c41743be2585e0925ff6f1a2625847de3c42c
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2020-10-26 20:38:17 +0500

    Added user model

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const app=express();
-const PORT=5000;
-const mongoose=require("mongoose")
-const {MONGOURI}=require("./keys")
+const app = express();
+const PORT = 5000;
+const mongoose = require("mongoose")
+const { MONGOURI } = require("./keys")
+//we are not exporting require that is why I am not creating an object here
+require("./models/user")
 
-mongoose.connect(MONGOURI,
-   {
-       useUnifiedTopology:true,
-       useNewUrlParser:true
-   }
-    )
-mongoose.connection.on("connected",()=>{
+mongoose.connect(MONGOURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+mongoose.connection.on("connected", () => {
     console.log("Mongo Database connected")
 })
-mongoose.connection.on("error",(err)=>{
-    console.log("Error Database connected",err)
+mongoose.connection.on("error", (err) => {
+    console.log("Error Database connected", err)
 })
 
-app.listen(PORT,()=>{
-    console.log("Server is up and running ",PORT);
+app.listen(PORT, () => {
+    console.log("Server is up and running ", PORT);
 })
