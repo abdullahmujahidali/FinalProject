795d9911d9979b3058c23329eb9b5dcec46e11a0
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-26 20:16:52 +0500

    Added custom middleware

--- server/app.js
+++ server/app.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app=express();
 const PORT=5000;
 
+const customMiddleware=(req,res,next)=>{
+    next()
+}
+
+app.use(customMiddleware)
+
 app.get("/",(req,res)=>{
     res.send("Server")
+})
+app.get("/about",customMiddleware,(req,res)=>{
+    res.send("About Page")
 })
 
 app.listen(PORT,()=>{
