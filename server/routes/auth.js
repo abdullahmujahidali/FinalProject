6fc27cfe0fc0b3b45568c7570b1909dcfe55a7d1
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2020-10-27 20:11:36 +0500

    MiddleWare added for protected routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,6 +5,12 @@ const User = mongoose.model("User")
 const bcrypt = require("bcryptjs")
 const jwt=require("jsonwebtoken")
 const {JWT_SECRET} = require("../keys")
+const requireLogin = require("../middleware/requireLogin")
+router.get("/protected",requireLogin,(req,res)=>{
+    res.send("midd");
+})
+
+
 router.post("/signup", (req, res) => {
     const { name, email, password } = req.body
     if (!email || !password || !name) {
