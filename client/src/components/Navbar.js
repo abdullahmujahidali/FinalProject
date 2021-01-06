997fd8a63ece3f9479fffa21bbfadf08da9cb12a
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2021-01-06 23:59:56 +0500

    Fetched data of posts on main page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,47 +1,65 @@
-import React from "react"
-import {Link} from "react-router-dom"
+import React, { useContext } from "react"
+import { Link } from "react-router-dom"
+import { UserContext } from "../App"
 import './Style.css';
 import "./main.js"
 // import {} from "../components/screens/Home"
-const NavBar=()=>{
-    return (
-        <div className="container-fluid p-0">
-        <nav className="navbar navbar-expand-lg">
-          <a className="navbar-brand" href="/">
-            <i className="fas fa-brain fa-2x mx-3"></i>BigBrains</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <i className="fas fa-align-right text-light"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <div className="mr-auto"></div>
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">HOME
-                  <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              {/* #section-1 */}
-              <li className="nav-item">
-                <Link className="nav-link" to="/section-1">ABOUT</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">PRICE</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">CONTACT</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/create">Create Post</Link>
-              </li>
-              <button type="button" className="btn btn-warning">
-               <Link to="/SignIn"> <i className="fas fa-sign-in-alt  mx-2"></i></Link>
-              </button>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    )
+const NavBar = () => {
+  const { state, dispatch } = useContext(UserContext)
+  const renderList = () => {
+    if (state) {
+      return [
+        <li className="nav-item">
+          <Link className="nav-link" to="/create">CREATE POST</Link>
+        </li>,
+        <li className="nav-item">
+          <Link className="nav-link" to="/profile">PROFILE</Link>
+        </li>,
+        <button type="button" className="btn btn-warning">
+          <Link to="/"> <i className="fas fa-sign-out-alt  mx-2"></i></Link>
+        </button>
+      ]
+    }
+    else {
+      return [
+        <li className="nav-item active">
+          <a className="nav-link" href="/">HOME
+          <span className="sr-only">(current)</span>
+          </a>
+        </li>,
+        <li className="nav-item">
+          <Link className="nav-link" to="/">ABOUT</Link>
+        </li>,
+        <li className="nav-item">
+          <Link className="nav-link" to="/">CONTACT</Link>
+        </li>,
+        <button type="button" className="btn btn-warning">
+          <Link to="/SignIn"> <i className="fas fa-sign-in-alt  mx-2"></i></Link>
+        </button>
+      ]
+
+    }
+  }
+
+  return (
+    <div className="container-fluid p-0">
+      <nav className="navbar navbar-expand-lg">
+        <Link  className="navbar-brand" to={state?"/PostHome":"/"}>
+          <i className="fas fa-brain fa-2x mx-3"></i>BigBrains</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <i className="fas fa-align-right text-light"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="mr-auto"></div>
+          <ul className="navbar-nav">
+            {renderList()}
+
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
 }
 
 export default NavBar;
--- client/src/components/screens/PostHome.js
+++ client/src/components/screens/PostHome.js
@@ -1,6 +1,17 @@
-import React from "react"
-
+import React, { useState, useEffect } from "react"
+import parse from "html-react-parser"
 const Footer = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch("/allpost", {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+            .then(result => {
+                setData(result.posts)
+            })
+    }, [])
     return (
         <>
             <section className="container">
@@ -10,85 +21,51 @@ const Footer = () => {
                 <p className="lead"><i className="fas fa-user"></i> Big Brains!</p>
 
                 <div className="posts">
-                    <div className="post bg-white p-1 my-1">
-                        <div>
-                            <a href="profile.html">
-                                <img
-                                    className="round-img"
-                                    src="https://pbs.twimg.com/media/DaooCaEU0AAdMFJ.jpg"
-                                    alt=""
-                                />
-                                <h4>John Doe</h4>
-                            </a>
-                        </div>
-                        <div>
-                            <p className="my-1">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-                                possimus corporis sunt necessitatibus! Minus nesciunt soluta
-                                suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-                                dolor? Illo perferendis eveniet cum cupiditate aliquam?
+                    {
+                        data.map(item => {
+                            return (
+                                <div className="post bg-white p-1 my-1" key={item._id}>
+                                    <div>
+                                        <a href="profile.html">
+                                            <img
+                                                className="round-img"
+                                                src={item.photo}
+                                                alt=""
+                                            />
+                                            <h4>{item.postedBy.name}</h4>
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <h2>{item.title}</h2>
+                                        <p className="my-1">
+                                        <img src={item.photo} alt=""></img>
+                                        {parse(item.body)}
             </p>
-                            <p className="post-date">
-                                Posted on 04/16/2019
+                                        <p className="post-date">
+                                            {item.subject}
             </p>
-                            <button type="button" className="btn btn-light">
-                                <i className="fas fa-thumbs-up"></i>
-                                <span>4</span>
-                            </button>
-                            <button type="button" className="btn btn-light">
-                                <i className="fas fa-thumbs-down"></i>
-                            </button>
-                            <a href="post.html" className="btn btn-primary">
-                                Discussion <span className='comment-count'>2</span>
-                            </a>
-                            <button
-                                type="button"
-                                className="btn btn-danger"
-                            >
-                                <i className="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
+                                        <button type="button" className="btn btn-light">
+                                            <i className="fas fa-thumbs-up"></i>
+                                            <span>4</span>
+                                        </button>
+                                        <button type="button" className="btn btn-light">
+                                            <i className="fas fa-thumbs-down"></i>
+                                        </button>
+                                        <a href="post.html" className="btn btn-primary">
+                                            Discussion <span className='comment-count'>3</span>
+                                        </a>
+                                        <button
+                                            type="button"
+                                            className="btn btn-danger"
+                                        >
+                                            <i className="fas fa-times"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
 
-                    <div className="post bg-white p-1 my-1">
-                        <div>
-                            <a href="profile.html">
-                                <img
-                                    className="round-img"
-                                    src="https://pbs.twimg.com/media/DaooCaEU0AAdMFJ.jpg"
-                                    alt=""
-                                />
-                                <h4>John Doe</h4>
-                            </a>
-                        </div>
-                        <div>
-                            <p className="my-1">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-                                possimus corporis sunt necessitatibus! Minus nesciunt soluta
-                                suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-                                dolor? Illo perferendis eveniet cum cupiditate aliquam?
-            </p>
-                            <p className="post-date">
-                                Posted on 04/16/2019
-            </p>
-                            <button type="button" className="btn btn-light">
-                                <i className="fas fa-thumbs-up"></i>
-                                <span>4</span>
-                            </button>
-                            <button type="button" className="btn btn-light">
-                                <i className="fas fa-thumbs-down"></i>
-                            </button>
-                            <a href="post.html" className="btn btn-primary">
-                                Discussion <span className='comment-count'>3</span>
-                            </a>
-                            <button
-                                type="button"
-                                className="btn btn-danger"
-                            >
-                                <i className="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
                 </div>
             </section>
         </>
