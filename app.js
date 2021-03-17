176abca036c17641cb2e8f92b4b5f629350872ea
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-03-17 14:12:09 +0500

    sadsa git commit -m sadsa

--- app.js
+++ app.js
@@ -31,14 +31,15 @@ app.use(require("./routes/post"))
 app.use(require("./routes/user"))
 
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(_dirname,'client','build','index.html'));
-    });
-}
 
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use('/', express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(_dirname,'client','build','index.html'));
+    }); 
+  }
 
 
 app.listen(PORT, () => {
