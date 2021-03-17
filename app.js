aec9af6b83a9e861f4bd73f519695aff4073db56
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-03-17 16:56:25 +0500

    updating component

--- app.js
+++ app.js
@@ -31,15 +31,15 @@ app.use(require("./routes/post"))
 app.use(require("./routes/user"))
 
 
-
-if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use('/', express.static("client/build"));
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static("client/build"));
     const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(_dirname,'client','build','index.html'));
-    }); 
-  }
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+
 
 
 app.listen(PORT, () => {
